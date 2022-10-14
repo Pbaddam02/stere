5f881a12257940f607252e6b2663a283ca23b958
BaddamPrashanth <prashanth.baddam@emburse.com>
2022-10-14 12:56:03 -0400

    added wait for page load on button

--- stere/fields/splinter/button.py
+++ stere/fields/splinter/button.py
@@ -1,3 +1,6 @@
+from stere.browser_spy import XHRSpy
+from stere.utils import _retry
+
 from .clickable import Clickable
 from ..decorators import stere_performer
 
@@ -14,3 +17,21 @@ class Button(Clickable):
         >>> purchase.click()
 
     """
+
+    xhr_spy = XHRSpy()
+
+    def wait_for_page_load(self):
+        """Wait for browser to finish loading content."""
+        return self.browser.evaluate_script(
+            "document.readyState === 'complete'")
+
+    def after(self):
+        """Wait for page to be silent."""
+        # Wait for page load
+        loaded = _retry(self.wait_for_page_load)
+        if not loaded:
+            raise TimeoutError('Page did not finish loading.')
+
+        # Wait for network requests
+        self.xhr_spy.add()
+        self.xhr_spy.wait_for_no_activity()
